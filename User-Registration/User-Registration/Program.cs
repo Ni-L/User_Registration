2c0f86fe4e321c24c1f047466028b5a90996e9b5
unknown <nilimawadal1@gmail.com>
2021-04-19 16:38:50 +0530

    [Nilima] Added Valide Mobile Number

--- User-Registration/User-Registration/Program.cs
+++ User-Registration/User-Registration/Program.cs
@@ -7,45 +7,49 @@ namespace User_Registration
     {
         static void Main(string[] args)
         {
-            //Entery point 
-
+            //Entry point for the Mobile Number
             Console.WriteLine("******Welcome To User Registration******");
-            Console.WriteLine();
-            string Pattern = "^([a-z]{3,})([.]{0,1}[a-z]*)@([a-z]{2}).([a-z]{2})([.]{1}[a-z]{2}){0,1}$"; //pattern using for email.
+            string Pattern = "^91\\s[1-9]{1}[0-9]{9}$"; //Pattern to get Number
+            //Input Array Numbers
+            string[] inputs1 = { "92 8978674532", "91 9146293697", "99922334455", "91 8907562431", "990088776655", "990088awek678543" };
 
-            Program p = new Program(); // create object
-            p.ValidName(Pattern);
+            Program p = new Program(); // Creating object for number 
+            p.Validnumber(inputs1, Pattern);
             Console.WriteLine();
             Console.Read();
         }
-
-        public void ValidName(string Pattern) //method to validate string
+        //Creating Method for Valid Number And Passes Arguments string type 
+        public void Validnumber(string[] arr, string Pattern)   
         {
             Console.WriteLine("Validation Of The Email");
+            //Creating Object of Regex 
             Regex regex = new Regex(Pattern);
-            IterateLoop(regex);  //calling method
+            //Calling method 
+            IterateLoop(arr, regex);  
         }
 
-        public void IterateLoop(Regex regex)
+        //Creating Method for iterateting 
+        public void IterateLoop(string[] arr, Regex regex)
         {
-            int i = 0; //initialize i value
-            while (i != 1)
-            {
-                Console.WriteLine("Enter Email");
-                string email = Console.ReadLine();
-                bool result = regex.IsMatch(email);  //call the IsMatch metod to determine whether a match is present
+            //Starting of for loop for checking upto Array length
+            for (int i = 0; i < arr.Length; i++)     
 
-                if (result == true)  ////check result is true or not using if and hence using bool
+            {
+                //Call the IsMatch metod to determine whether a match is present
+                bool result = regex.IsMatch(arr[i]);
+                //Check result is true or not using if and hence using bool
+                if (result == true)  
                 {
-                    Console.WriteLine("Valid email");
-                    i = 1;
+                    Console.WriteLine(arr[i] + "->" + "Valid");//if true print 
+
                 }
                 else
                 {
-                    Console.WriteLine("Enter email in this format : abc.xyz@bl.co.in");
+                    Console.WriteLine(arr[i] + "->" + "Invalid MobileNumber");//if false print
                 }
-
+                //End of If Else Loop
             }
+            //End of for Loop
         }
     }
 }
